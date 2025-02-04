59e57bd1dbde6f5e0e56dc0a62d876f4e5c2795e
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2025-02-04 17:55:38 +0900

    포폴 추가

--- src/redux/states.js
+++ src/redux/states.js
@@ -3,7 +3,7 @@ const listState = {
     list_kind1: [],  // LIST ALL, WEB, APP, HYBRID
     list_kind2: [],  // LIST ...ETC skill kinds
     kind1: ['ALL', 'WEB', 'APP', 'HYBRID'],
-    kind2: ['react', 'vue', 'nodejs', 'graphql', 'apollo', 'nextjs', 'reactNative', 'jsp', 'sql', 'naverCloud', 'mysql', 'mongodb', 'typescript', 'R3F'],
+    kind2: ['react', 'vue', 'nodejs', 'graphql', 'apollo', 'nextjs', 'reactNative', 'jsp', 'sql', 'naverCloud', 'mysql', 'mongodb', 'typescript', 'R3F', 'tailwind', 'recoil'],
     user_chk_kind2: [],
     inputRef: [],
     listCounting: 5, // 몇개까지 보여줄지 셋팅, more버튼 누를때마다 5개씩 추가
